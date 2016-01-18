0350d5e8d2389c0a5158b4fe7cdefdbf9677f885
jiexu10 <jie.xu.10@gmail.com>
2016-01-18 14:37:24 -0500

    restaurant sign in test with company name custom validation complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,13 @@ class User < ActiveRecord::Base
   validates :role,
     presence: true,
     inclusion: { in: ROLES.map { |role| role[0] } }
+  validate :company_name_for_restaurant
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
+
+  def company_name_for_restaurant
+    if role == '2' && company_name.blank?
+      errors.add(:company_name, "Company name can't be blank for restaurants!")
+    end
+  end
 end
