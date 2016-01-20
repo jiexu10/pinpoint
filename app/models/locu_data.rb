0220dd6e9ec082cbb1611a9459a81ae5caaaac90
jiexu10 <jie.xu.10@gmail.com>
2016-01-19 19:58:36 -0500

    locu api wrapper working with search functionality and tests

--- app/models/locu_data.rb
+++ app/models/locu_data.rb
@@ -1,14 +1,7 @@
-require 'net/http'
-require 'pry'
-require 'openssl'
-require 'json'
-require 'httparty'
-
 class LocuData
   include HTTParty
-  attr_reader :data, :name, :location, :categories
-
-  base_uri "https://api.locu.com"
+  attr_reader :data, :name, :location, :categories, :open_hours, :menus
+  attr_accessor :venue_queries
 
   def initialize(query = {})
     @name = query['name']
@@ -17,86 +10,25 @@ class LocuData
     @open_hours = query['open_hours']
     @menus = query['menus']
     @data = nil
-  end
-
-  def get_posts
-    uri_string = "https://api.locu.com"
-    uri = URI.parse(uri_string)
-    message = '{
-      "api_key": "0a617b50e355f12544adc19e76d4714d0d8a6a46",
-      "fields": [ "name" ],
-      "venue_queries": [
-        {
-          "name": "bistro central parc"
-        }
-      ]
-    }'
-    # binding.pry
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Post.new("/v2/venue/search", initheader = {'Content-Type' =>'application/json'})
-    # request.add_field('Content-Type', 'application/json')
-    request.body = message
-    # response = http.start { |http| http.request(request) }
-
-    # uri = URI.parse('https://api.locu.com/v2/venue/search')
-    # response = Net::HTTP.post_form(uri, message)
-    # http = Net::HTTP.new(uri.host, uri.port)
-    # request = Net::HTTP::Post.new(uri.request_uri)
-    # request.set_form_data(message)
-
-    response = http.request(request)
-    binding.pry
-    @data = JSON.parse(response)
-    binding.pry
-  end
-
-  def get_stuff
-    uri_string = "https://api.locu.com"
-    uri = URI.parse(uri_string)
-    message = %Q({
-      "api_key": "#{ENV['LOCU_API_KEY']}",
-      "fields": [ "name", "categories", "location", "menus" ],
-      "venue_queries": [
-        {
-          "location": {
-              "locality": "Boston"
-            },
-          "categories": {
-              "str_id": "restaurants"
-          }
-        }
-      ]
-    })
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Post.new("/v2/venue/search", initheader = {'Content-Type' =>'application/json'})
-    request.body = message
-
-    response = http.request(request)
-    @data = JSON.parse(response.body)
-    binding.pry
+    @venue_queries = { 'categories' => { 'str_id' => 'restaurants' } }
   end
 
   def build_venue_queries
-    venue_queries = {}
-    venue_queries['name'] = @name if @name
-    venue_queries['categories']['str_id'] = @categories if @categories
-    venue_queries['menus']['$present'] = true if @menu
-    venue_queries['open_hours'] = @open_hours if @open_hours
-    venue_queries['location']['locality'] = @location['locality'] if @location['locality']
-    venue_queries['location']['postal_code'] = @location['postal_code'] if @location['postal_code']
-    venue_queries['location']['geo']['$in_lat_lng_radius'] = @location['lat_lng_radius'] if @location['lat_lng_radius']
-    if venue_queries.empty?
-      venue_queries['location']['locality'] = 'Boston'
+    venue_queries['name'] = name if name
+    build_menus_query
+    venue_queries['open_hours'] = open_hours if open_hours
+    venue_queries['categories']['str_id'] = categories if categories
+    build_location_query
+    unless venue_queries['location']
+      venue_queries['location'] = {}
+      venue_queries['location']['locality'] = IpApi.new.data['city']
     end
     venue_queries
   end
 
   def search_function
-    default_fields = [ "name", "description", "website_url", "menus", "open_hours", "categories", "location", "contact", "delivery" ]
+    default_fields = [ "name", "description", "website_url", "menus",
+      "open_hours", "categories", "location", "contact", "delivery" ]
     post_url = "https://api.locu.com/v2/venue/search"
     message = {
       "api_key" => "#{ENV['LOCU_API_KEY']}",
@@ -106,23 +38,29 @@ class LocuData
     @data = HTTParty.post(post_url,
       body: message.to_json,
       headers: {'Content-Type' =>'application/json'} )
-    binding.pry
   end
-  # def parse
-  #   unless @data
-  #     get_posts
-  #   end
-  #   output = []
-  #   @data['data']['children'].each do |post|
-  #     unless post['data']['stickied']
-  #       title = post['data']["title"]
-  #       url = "https://www.reddit.com" + post['data']['permalink']
-  #       descrip = post['data']['selftext']
-  #       output << { 'Title' => title, 'URL' => url, 'Description' => descrip }
-  #     end
-  #   end
-  #   output
-  # end
-end
 
-binding.pry
+  private
+
+  def build_menus_query
+    if menus
+      venue_queries['menus'] = {}
+      venue_queries['menus']['$present'] = true
+    end
+  end
+
+  def build_location_query
+    if location
+      venue_queries['location'] = {}
+      if location['locality']
+        venue_queries['location']['locality'] = location['locality']
+      elsif location['postal_code']
+        venue_queries['location']['postal_code'] = location['postal_code']
+      elsif location['lat_lng_radius']
+        venue_queries['location']['geo'] = {}
+        venue_queries['location']['geo']['$in_lat_lng_radius'] = location['llr']
+      end
+    end
+  end
+
+end
--- spec/models/locu_data_spec.rb
+++ spec/models/locu_data_spec.rb
@@ -1,37 +1,75 @@
 require 'rails_helper'
 
-RSpec.describe LocuData type: :model, vcr: true do
-  let(:locu_data) { LocuData.new }
-  it 'receives json data from locu' do
-    expect(lpt_data.get_posts['kind']).to eq('Listing')
-  end
-
-  it 'receives data from LPT subreddit' do
-    lpt_data.get_posts['data']['children'].each do |post|
-      expect(post['data']['subreddit']).to eq('LifeProTips')
+RSpec.describe LocuData, type: :model, vcr: true do
+  it 'successfully receives json data from locu with default parameters' do
+    locu_data = LocuData.new
+    locu_data.search_function
+    expect(locu_data.data['status']).to eq('success')
+    locu_data.data['venues'].each do |venue|
+      expect(venue['location']['locality']).to eq(IpApi.new.data['city'])
+      expect(venue['categories'].any? { |category| category['str_id'] == 'restaurants' }).to eq(true)
     end
   end
 
-  it 'parses and the posts' do
-    lpt_data.parse.each do |post|
-      expect(post['Title']).to_not be_blank
-      expect(post['URL']).to_not be_blank
+  it 'can search by name' do
+    locu_data = LocuData.new( { 'name' => 'bistro central parc'} )
+    locu_data.search_function
+    expect(locu_data.data['venues'].count).to eq(1)
+    expect(locu_data.data['venues'][0]['locu_id']).to eq('3ac33e6306c52465e30a')
+    expect(locu_data.data['venues'][0]['name']).to eq('Bistro Central Parc')
+  end
+
+  it 'can search by open times' do
+    locu_data = LocuData.new( { 'open_hours' => { 'monday' => ['18:00'] } } )
+    t = Time.local(2016, 1, 19, 18, 0, 0)
+    locu_data.search_function
+    locu_data.data['venues'].each do |venue|
+      expect(venue['open_hours'].has_key?('monday')).to eq(true)
+      expect(venue['open_hours']['monday'][0][0].to_i).to be <= t.strftime("%H").to_i
     end
   end
 
-  it 'adds entries from LPT subreddit into the database' do
-    lpt_data.seed_db
-    lpt_data.parse.each do |post|
-      expect(Lifehack.find_by(url: post["URL"])).to be_a(Lifehack)
+  it 'can search by location, locality' do
+    locu_data = LocuData.new( { 'location' => { 'locality' => 'Cambridge' } } )
+    locu_data.search_function
+    locu_data.data['venues'].each do |venue|
+      expect(venue['location']['locality']).to eq('Cambridge')
     end
   end
 
-  it 'adds entries with the appropriate account' do
-    lpt_data.seed_db
-    lifehacks = Lifehack.all
-    lifehacks.each do |lifehack|
-      expect(lifehack.creator.first_name).to eq 'Lifehack'
-      expect(lifehack.creator.last_name).to eq 'Bot'
+
+  it 'can search by location, postal code' do
+    locu_data = LocuData.new( { 'location' => { 'postal_code' => '02111' } } )
+    locu_data.search_function
+    locu_data.data['venues'].each do |venue|
+      expect(venue['location']['postal_code']).to eq('02111')
+    end
+  end
+
+  it 'can search by location, latitude-longitude-radius' do
+    locu_data = LocuData.new( { 'location' => { 'lat_lng_radius' => [42.359464, -71.061292, 500] } } )
+    locu_data.search_function
+    locu_data.data['venues'].each do |venue|
+      expect(venue['location']['locality']).to satisfy("be in Boston or Cambridge") do |location|
+        location == 'Boston' || location == 'Cambridge' || location == 'Dorchester'
+      end
+    end
+  end
+
+  it 'can search by menu' do
+    locu_data = LocuData.new( { 'menus' => true } )
+    locu_data.search_function
+    locu_data.data['venues'].each do |venue|
+      expect(venue.has_key?('menus')).to eq(true)
+    end
+  end
+
+  it 'can search by categories' do
+    locu_data = LocuData.new( { 'categories' => 'pizza' } )
+    locu_data.search_function
+    locu_data.data['venues'].each do |venue|
+      expect(venue['categories'].any? { |category| category['str_id'] == 'pizza' }).to eq(true)
+      expect(venue['categories'].any? { |category| category['str_id'] == 'restaurants' }).to eq(true)
     end
   end
 end
