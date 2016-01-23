27a90a63fe70f9e1bc4adbed5fa016e07dfc1d44
jiexu10 <jie.xu.10@gmail.com>
2016-01-23 18:44:33 -0500

    wip styling main page, switching branches to add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+working_restaurants = [
+  "Boston Beer Garden",
+  "Betty's Wok & Noodle Diner"
+]
