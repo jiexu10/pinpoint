6266119a5ae03e01f73f1c4c80a45478439a591c
jiexu10 <jie.xu.10@gmail.com>
2016-01-25 02:04:30 -0500

    fixed seeder test from change

--- spec/seeders/seeder_spec.rb
+++ spec/seeders/seeder_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Seeder, type: :model, vcr: true do
   it 'can seed the database with the expected information' do
     Seeder.seed_restaurants
-    expect(Restaurant.all.count).to eq(6)
+    expect(Restaurant.all.count).to eq(5)
     Seeder::WORKING_RESTAURANTS.each do |restaurant|
       expect(Restaurant.find_by(company_name: restaurant)).to be_a(Restaurant)
     end
