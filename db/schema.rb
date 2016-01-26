7c4a112d92aaa1f4886b21110cd2a4598c69af3e
jiexu10 <jie.xu.10@gmail.com>
2016-01-26 12:06:07 -0500

    wip added migration for driver role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125171857) do
+ActiveRecord::Schema.define(version: 20160126164404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,21 +130,24 @@ ActiveRecord::Schema.define(version: 20160125171857) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name",                          null: false
-    t.string   "last_name",                           null: false
+    t.string   "first_name",                                                        null: false
+    t.string   "last_name",                                                         null: false
     t.string   "company_name"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                            default: "",     null: false
+    t.string   "encrypted_password",                               default: "",     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                                    default: 0,      null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                                        null: false
+    t.datetime "updated_at",                                                        null: false
+    t.string   "role",                                             default: "user", null: false
+    t.decimal  "latitude",               precision: 18, scale: 15
+    t.decimal  "longitude",              precision: 18, scale: 15
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
