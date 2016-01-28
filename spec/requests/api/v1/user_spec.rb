e19d4959f400392a88ee910c31f4a7fbac71b35e
jiexu10 <jie.xu.10@gmail.com>
2016-01-27 19:52:32 -0500

    finished api tests

---
+++ spec/requests/api/v1/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::UsersController, vcr: true do
+  let!(:driver) { FactoryGirl.create(:user, :driver) }
+
+  it 'can retrieve latitude and longitude data' do
+    driver.update_attributes(latitude: 42.3456789, longitude: -67.8912345)
+    expect(driver.valid?).to eq(true)
+
+    new_lat = "12.3456789"
+    new_lng = "23.4567891"
+    params = { :id => driver.id, :lat => new_lat, :lng => new_lng }
+    patch "/api/v1/users/#{driver.id}", params, format: :json
+
+    json = JSON.parse(response.body)
+    expect(json['lat']).to eq(new_lat)
+    expect(json['lng']).to eq(new_lng)
+  end
+end
