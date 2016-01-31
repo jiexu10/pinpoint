b106a25b110a532f078b9a8d80c4413f799eacc6
jiexu10 <jie.xu.10@gmail.com>
2016-01-30 19:02:20 -0500

    added test for driver location clear

--- spec/requests/api/v1/user_spec.rb
+++ spec/requests/api/v1/user_spec.rb
@@ -16,4 +16,17 @@ RSpec.describe Api::V1::UsersController, vcr: true do
     expect(json['lat']).to eq(new_lat)
     expect(json['lng']).to eq(new_lng)
   end
+
+  it 'can clear latitude and longitude data' do
+    driver.update_attributes(latitude: 42.3456789, longitude: -67.8912345)
+    expect(driver.valid?).to eq(true)
+
+    params = { id: driver.id, request: 'clear' }
+    patch "/api/v1/users/#{driver.id}", params, format: :json
+
+    json = JSON.parse(response.body)
+    expect(json['result']).to eq('cleared')
+    expect(User.first.latitude).to be(nil)
+    expect(User.first.longitude).to be(nil)
+  end
 end
